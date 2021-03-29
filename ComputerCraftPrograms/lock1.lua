0c56a5706df7c6c0dbaa5ce4ac07cc035b3e5c6f
switchchan <whowontcareatall@gmail.com>
2021-03-29 15:53:42 +0200

    Update lock1.lua

--- ComputerCraftPrograms/lock1.lua
+++ ComputerCraftPrograms/lock1.lua
@@ -1,69 +1,58 @@
--- the list of allowed disks... it uses their ID, since in survival you will never have a disk with the same ID.
--- to add more, add the id on the left, followed by     = true,
-local allowedDisksIds = {
-  1 = true,
-  2 = true,
-  3 = true,
-}
+--Allowed floppy ID. This code only allows 1 floppy to be inserted, but you can easily modify it to have multiple allowed.
+allowedID = 92
  
--- the side to check for disks
-local diskSide = 'bottom'
--- the side the door, or the cable to the door is on
-local rsSide = 'right'
+--Variable for which side the redstone will be at
+redstoneSide = 'right'
  
--- this checks if it is an accepted disk 
-local function isValid()
-  -- we get the disk ID
-  local diskId = disk.getDiskID and disk.getDiskID(diskSide) or disk.getID(diskSide) -- support for old versions of ComputerCraft
-  -- we return the value from the allowed disks table (it will be true or nil/false)
-  return allowedDisksIds[diskId]
-end
+--Variable for which side your disk drive will be on. You can also change it to networked positions
+diskSide = 'bottom'
  
--- wait for x amount of time, it is a termination safe sleep function
-local function wait( dur )
-  dur = type(dur) == 'number' and dur or 1
-  local t = os.startTimer(dur)
-  repeat
-    local _, p = os.pullEventRaw('timer')
-  until p == t
-end
- 
--- the main text while it is waiting for a disk
-local function text()
-  term.clear()
-  term.setCursorPos(1,1)
-  print("Please insert disk to enter...")
-end
- 
--- this is the main function
-local function diskCheck()
-  -- infinitely loop
-  while true do
-    -- draw the GUI
-    text()
-    -- wait here until a disk is inserted
-    local _, side = os.pullEvent('disk')
-    -- if the disk inserted is on the correct side, and it has a valid id
-    if side == diskSide and isValid() then
-      -- give entry
-      rs.setOutput(rsSide, true)
-      -- tell them they have entry
-      term.setCursorPos(1,2)
-      write("ACCESS GRANTED")
-      -- eject their disk
-      disk.eject(diskSide)
-      -- wait 3 seconds
-      wait(3)
-      -- close the door
-      rs.setOutput(rsSide, false)
+--Checks to see if it's the correct disk
+local function isCorrect()
+    diskID = disk.getID(diskSide)
+    if diskID == allowedID then
+        return true
     else
-      -- if their card wasn't valid, or they inserted in a disk drive on another side, eject the disk
-      disk.eject(side)
-      -- wait for 10 seconds
-      wait(10)
+        return false
     end
-  end
 end
  
--- run the main function
-diskCheck()
+--Function that just waits a given number of seconds
+local function wait( dur )
+    dur = type(dur) == 'number' and dur or 1
+    local t = os.startTimer(dur)
+    repeat
+        local _, p = os.pullEventRaw('timer')
+    until p == t
+end
+ 
+--Standby text
+local function text()
+    term.clear()
+    term.setCursorPos(1,2)
+    print("Insert Disk")
+end
+ 
+--waits until a disk is inserted and then runs isCorrect() in order to figure out what to do next
+local function checkDisk()
+    while true do
+        text()
+        local _, side = os.pullEvent('disk')
+        if isCorrect() == true then
+            rs.setOutput(redstoneSide, true)
+            term.setCursorPos(1,2)
+            term.clear()
+            print("GRANTED")
+            disk.eject(diskSide)
+            wait(3)
+                rs.setOutput(redstoneSide, false)
+        else
+            disk.eject(diskSide)
+            term.clear()
+            print("Denied.")
+            wait(10)
+        end
+    end
+end
+ 
+checkDisk()
